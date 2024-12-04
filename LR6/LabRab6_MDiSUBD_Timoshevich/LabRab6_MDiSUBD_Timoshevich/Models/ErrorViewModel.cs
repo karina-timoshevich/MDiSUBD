8e4b105196642ae959456fbf3104101e8fc3b917
Karina Timoshevich <karina03tima@gmail.com>
2024-12-05 00:56:54 +0300

    start project

---
+++ LR6/LabRab6_MDiSUBD_Timoshevich/LabRab6_MDiSUBD_Timoshevich/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LabRab6_MDiSUBD_Timoshevich.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
